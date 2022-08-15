62be2516c295eac5a8f12e8e6a715080854a8dd1
bimapw4 <bimapw4@gmail.com>
2022-08-15 14:56:38 +0700

    update config

--- config/database.go
+++ config/database.go
@@ -13,7 +13,7 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	db, err := gorm.Open(mysql.Open(os.Getenv("DB_USERNAME")+":"+os.Getenv("DB_PASSWORD")+"@tcp("+os.Getenv("DB_HOST")+":"+os.Getenv("DB_PORT")+")/"+os.Getenv("DB_DATABASE")+"?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{
+	db, err := gorm.Open(mysql.Open(os.Getenv("MYSQL_USERNAME")+":"+os.Getenv("MYSQL_PASSWORD")+"@tcp("+os.Getenv("MYSQL_HOST")+":"+os.Getenv("MYSQL_PORT")+")/"+os.Getenv("MYSQL_DATABASE")+"?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{
 		Logger: logger.New(
 			log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
 			logger.Config{
